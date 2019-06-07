69532d3a25d98ae477bf586b8cefe7f436d3ea9d
Harry Gothold <harry.gothold@kurtgeiger.com>
2019-06-07 17:18:29 +0100

    started adding MeetPlayers section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Featured from "./featured";
 import MatchesHome from "./matches";
+import MeetPlayers from "./MeetPlayers/Cards";
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <MatchesHome />
+      <MeetPlayers />
     </div>
   );
 };
