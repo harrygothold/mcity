86cb25859426fe78e2c4c830efacfaac2c66f2e6
Harry Gothold <harry.gothold@kurtgeiger.com>
2019-06-19 09:04:05 +0100

    Added auth component and signin functionality

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "./Hoc/Layout";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/home";
+import SignIn from "./components/auth";
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path='/sign_in' exact component={SignIn} />
       </Switch>
     </Layout>
   );
